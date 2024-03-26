804911111f4060d165bbbae79c9c5fae46302c75
mihalisgol <mihail.lipin.98@mail.ru>
2024-03-27 03:11:17 +0800

    add function

--- str_func.py
+++ str_func.py
@@ -4,3 +4,10 @@ def convert_to_uppercase(input_str):
     return input_str.upper()
 
 help(convert_to_uppercase)
+
+
+def capitalize_first_letters(input_str):
+    "Добавлена новая функия"
+    return ' '.join(word.capitalize() for word in input_str.split())
+help(capitalize_first_letters)
+
